8cb445230aa323e62a782150fd7ec35648739477
Lorenzo Pisani <zeelot3k@gmail.com>
2011-02-10 12:59:14 -0700

    adding an asset config group

---
+++ config/assets.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array
+(
+	'admin-theme' => array
+	(
+		array('style', 'media/ko/css/compiled/admin-theme/styles/'.Kohana::APP_VERSION.'.css', 'head'),
+	),
+);
